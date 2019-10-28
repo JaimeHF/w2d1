9756f0db1cb513c285f78661e7f1ebb9fb646bf5
dani <danivicario@dvicario.es>
2019-10-28 10:21:41 +0100

    added cart example with DOM selection

---
+++ index.js
@@ -0,0 +1,44 @@
+class Product {
+  constructor(name, price) {
+    if (price < 0) throw new Error("price cannot be negative");
+    this.name = name;
+    this.price = Math.round(Math.random() * 500);
+  }
+}
+
+let productsInCart = [
+  new Product("roomba hoover", 200),
+  new Product("thomson fridge", 1000)
+];
+
+function updateCartElements() {
+  const totalItemsDOMEl = document.querySelector("#cart .total-items");
+
+  totalItemsDOMEl.innerHTML = productsInCart.length;
+}
+
+updateCartElements();
+
+document.querySelector("#add-to-cart").onclick = function() {
+  productsInCart.push(new Product("new Item", 200));
+
+  updateCartElements();
+  displayCartContents();
+};
+
+let cartDOMEl = document.querySelector("#cart");
+
+function displayCartContents() {
+  let cartContentsDOMEl = document.querySelector("#cart-contents");
+
+  cartContentsDOMEl.innerHTML = "";
+
+  productsInCart.forEach(product => {
+    const productLIDOMEl = document.createElement("li");
+    productLIDOMEl.innerHTML = `<span>${product.name} -- ${product.price} eur</span>`;
+
+    cartContentsDOMEl.appendChild(productLIDOMEl);
+  });
+}
+
+cartDOMEl.onclick = displayCartContents;
