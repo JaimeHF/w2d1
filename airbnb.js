f5228771df464908b8f06c32a60074baa51dd62e
dani <danivicario@dvicario.es>
2019-10-28 14:27:48 +0100

    added all

--- airbnb.js
+++ airbnb.js
@@ -19,12 +19,18 @@ let sortPriceAscending = false;
 document.querySelector("#price-sorter").onclick = function() {
   sortPriceAscending = !sortPriceAscending;
 
+  document.querySelector("#price-sorter .sort").style.display = "inline-block";
+  document.querySelector("#name-sorter .sort").style.display = "none";
+
   if (sortPriceAscending) {
+    document.querySelector("#price-sorter .sort").className = "sort ascending";
+
     rooms.sort(function(room1, room2) {
       if (room1.price > room2.price) return -1;
       return 1;
     });
   } else {
+    document.querySelector("#price-sorter .sort").className = "sort";
     rooms.sort(function(room1, room2) {
       if (room1.price > room2.price) return 1;
       return -1;
@@ -37,27 +43,34 @@ document.querySelector("#price-sorter").onclick = function() {
 let sortNameAscending = false;
 
 document.querySelector("#name-sorter").onclick = function() {
-    sortNameAscending = !sortNameAscending;
+  sortNameAscending = !sortNameAscending;
+
+  document.querySelector("#price-sorter .sort").style.display = "none";
+  document.querySelector("#name-sorter .sort").style.display = "inline-block";
 
   if (sortNameAscending) {
+    document.querySelector("#name-sorter .sort").className = "sort ascending";
     // as per the LU JS | Data Types in JavaScript - number & string
     rooms.sort(function(room1, room2) {
-        return room1.name.localeCompare(room2.name);
+      return room1.name.localeCompare(room2.name);
     });
   } else {
+    document.querySelector("#name-sorter .sort").className = "sort";
+
     rooms.sort(function(room1, room2) {
-        return room2.name.localeCompare(room1.name);
+      return room2.name.localeCompare(room1.name);
     });
   }
 
   drawRooms();
 };
 
-function drawRooms() {
+// predefined values. If not filteredRooms is passed, get by default rooms
+function drawRooms(filteredRooms = rooms) {
   let roomsDOMEl = document.querySelector("#rooms tbody");
   roomsDOMEl.innerHTML = "";
 
-  rooms.forEach(room => {
+  filteredRooms.forEach(room => {
     let roomDOMEl = document.createElement("tr");
     roomDOMEl.innerHTML = `
               <td>${room.name}</td>
@@ -67,3 +80,11 @@ function drawRooms() {
     roomsDOMEl.appendChild(roomDOMEl);
   });
 }
+
+document.querySelector("#search").onkeyup = function(e) {
+  let filteredRooms = rooms.filter(
+    room => room.name.toUpperCase() === e.target.value.toUpperCase()
+  );
+
+  drawRooms(filteredRooms);
+};
